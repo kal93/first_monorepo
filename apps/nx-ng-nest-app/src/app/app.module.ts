5a0e44d773420d9040ce32265386f76a9539cbba
kal93 <maaz2dt@gmail.com>
2021-02-15 13:22:02 +0530

    organize imports and correct widget details path

--- apps/nx-ng-nest-app/src/app/app.module.ts
+++ apps/nx-ng-nest-app/src/app/app.module.ts
@@ -1,38 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
 import {
-  CoreDataModule,
-  coreDataRoutes,
+  CoreDataModule
 } from '@production-nx-angular/core-data';
 import {
-  CoreStateModule,
-  coreStateRoutes,
+  CoreStateModule
 } from '@production-nx-angular/core-state';
 import {
-  MaterialModule,
-  materialRoutes,
+  MaterialModule
 } from '@production-nx-angular/material';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { RouttingModule } from './routting.module';
-import { WidgetsComponent } from './widgets/widgets.component';
+import { WidgetDetailsComponent } from './widgets/widget-details/widget-details.component';
 import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
-import { WidgetDetailsComponent } from './widget-details/widget-details.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+
 
 @NgModule({
-  declarations: [AppComponent, WidgetsComponent, WidgetsListComponent, WidgetDetailsComponent],
+  declarations: [
+    AppComponent, 
+    HomeComponent,
+    WidgetDetailsComponent, 
+    WidgetsComponent,
+    WidgetsListComponent, 
+  ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot({}, {}),
+    BrowserModule,
     CoreDataModule,
     CoreStateModule,
+    HttpClientModule,
     MaterialModule,
     RouttingModule,
+    // StoreModule.forRoot({}, {}),
   ],
   providers: [],
   bootstrap: [AppComponent],
