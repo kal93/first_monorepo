ca3bb3df476bcd44d6ab430b0fd0fedb368c2af9
kal93 <maaz2dt@gmail.com>
2021-02-15 22:23:54 +0530

    add swagger for nest

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,29 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app/app.module';
+
+/**
+ * 
+ * @description configureSwagger is a basic configuration copied from docs
+ */
+const configureSwagger = (app) => {
+  const options = new DocumentBuilder()
+                    .setTitle('Production Nest API')
+                    .setDescription('REST API for prodcution nx app')
+                    .setVersion('1.0')
+                    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.enableCors();
   app.setGlobalPrefix(globalPrefix);
+  configureSwagger(app);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
