346b92c42f081adb50819c8fb23842b9330b5fbd
kal93 <maaz2dt@gmail.com>
2021-02-15 21:12:21 +0530

    first nest api with mock data

--- apps/api/src/widgets/widgets.service.ts
+++ apps/api/src/widgets/widgets.service.ts
@@ -1,15 +1,39 @@
+import { Widget } from '@fem/api-interfaces';
 import { Injectable } from '@nestjs/common';
 import { CreateWidgetDto } from './dto/create-widget.dto';
 import { UpdateWidgetDto } from './dto/update-widget.dto';
 
 @Injectable()
 export class WidgetsService {
+  widgets: Widget[] = [
+    {
+      "id": "1",
+      "title": "Remote mock widget 1 FTW",
+      "description" : "Pending..."
+  },
+  {
+      "id": "2",
+      "title": "Remote mock widget 2 FTW",
+      "description" : "Pending..."
+  },
+  {
+      "id": "3",
+      "title": "Remote mock widget 3 FTW",
+      "description" : "Completed..."
+  },
+  {
+      "id": "4",
+      "title": "Remote mock widget 4 FTW",
+      "description" : "Pending..."
+  }
+  ];
+
   create(createWidgetDto: CreateWidgetDto) {
     return 'This action adds a new widget';
   }
 
   findAll() {
-    return `This action returns all widgets`;
+    return this.widgets;
   }
 
   findOne(id: number) {
